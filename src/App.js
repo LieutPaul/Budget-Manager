dc6821afbbaf1b3097e27e1db540699cf5e8f084
LieutPaul <Vikas.Kalyanapuram@iiitb.ac.in>
2023-02-08 22:46:16 +0530

    Commit

--- src/App.js
+++ src/App.js
@@ -2,7 +2,14 @@ import './App.css';
 function App() {
   return (
     <div className="container">
- 
+      <div className="row">
+        <div className="col-6">
+            Hello1
+        </div> 
+        <div className="col-6">
+            Hello2
+        </div>
+      </div>  
     </div>
   );
 }
